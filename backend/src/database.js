aaa576e18b605ccbe6305ffd2636035617b07264
Kevin Martinez <al3xmr@Ubuntu24.10-MR>
2024-11-24 18:39:20 -0500

    Soluciona la conexion a la BD de Atlas

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-//Cadena de conexión
-//Usa la variable de entorno de MONGODB_URI
-//Si no la cuentra, usa una base llamada 'dbtest'
-const URI = process.env.MONGODB_URI
-						
-						
+// Cadena de conexión
+// Usa la variable de entorno de MONGODB_URI o una base local por defecto
+const URI = process.env.MONGODB_URI || "mongodb://localhost:27017/dbtest";
+console.log(URI);
 
-mongoose.connect(URI);
-
-const connection = mongoose.connection();
-
-connection.once('open', ()=>{
-	console.log('La base de datos ha sido conectada: ', URI);
-});
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("La base de datos ha sido conectada: ", URI))
+  .catch((err) =>
+    console.error("Error al conectar con la base de datos: ", err)
+  );
