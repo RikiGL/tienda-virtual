cad6bfcbb3858f322cf7423dd0d471a1110f3e76
170025 <andy172641@gmail.com>
2024-12-09 21:52:50 -0500

    Creacion de funcion para encriptacion de contraseñas

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 //Llama a los modulos que instalamos mediante npm
 const express = require("express");
 const cors = require("cors");
+//const bcryptRoute = require('./routes/bcryptRoute');
 const app = express(); //En app se almacena las funcionalidad de express
 //const clienteRoutes = require("./routes/cliente.route");
 
@@ -13,7 +14,9 @@ app.set("port", process.env.PORT || 4000);
 app.use(cors()); // Para hacer consultas entre servidores
 app.use(express.json()); // Envio de JSON cuando se hace peticiones
 
+
 //rutas
+
 app.get("/", (req, res) => {
   res.send("Bienvenido a la API Rest Full");
 });
@@ -21,7 +24,7 @@ app.get("/", (req, res) => {
 
 //app.use("/api/clientes", clienteRoutes);
 
-
+//app.use('/auth', bcryptRoute);
 //rutas para la API de clientes
 app.use("/api/clientes", require("./routes/cliente.route"));
 
@@ -31,5 +34,6 @@ app.use("/api/facturas", require("./routes/factura.route"));
 //rutas para la API de productos
 app.use("/api/productos", require("./routes/productos.route"));
 
+//app.use("/a")
 //Se exporta el app para que sea utilizado en otras partes del proyecto
 module.exports = app;
