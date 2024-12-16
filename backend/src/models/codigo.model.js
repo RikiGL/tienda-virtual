a46a4d905c5f0e5ffe3dd00b8e7351b6e823287e
Kevin Martinez <94004062+Al3xMR@users.noreply.github.com>
2024-12-15 23:13:18 -0500

    Revert "Mafer"

--- backend/src/models/codigo.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const securityCodeSchema = new mongoose.Schema({
-    email: { type: String, required: true },
-    code: { type: String, required: true },
-    expiration: { type: Date, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model("SecurityCode", securityCodeSchema);
